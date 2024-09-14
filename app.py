201e99acbabb3269b9cb155cae5a536c5a94c3a4
Testeprototico <prototicoempreend@gmail.com>
2024-09-14 18:13:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import subprocess
 import threading
 import time
+import os
 from flask import Flask, send_file
 
 app = Flask(__name__)
@@ -9,12 +10,14 @@ HASHCAT_PATH = 'hashcat'
 LOG_FILE = 'hashcat.log'
 
 def run_hashcat():
-    command = [HASHCAT_PATH, '-m', '1000', '-a', '3',  'hash.txt']
+    command = [HASHCAT_PATH, '-m', '1000', '-a3', 'hash.txt', '?a?a?a?a']
     with open(LOG_FILE, 'w') as log_file:
-        log_file.write("Iniciando Hashcat...\n")  # Adicione uma mensagem inicial
+        log_file.write("Iniciando Hashcat...\n")
+        log_file.write(f"Uso de memória antes: {os.popen('free -m').read()}\n")
         process = subprocess.Popen(command, stdout=log_file, stderr=subprocess.STDOUT, text=True)
         process.wait()  # Espera o Hashcat terminar
-        log_file.write(f"Hashcat terminou com o código de saída {process.returncode}\n")  # Adiciona código de saída
+        log_file.write(f"Uso de memória depois: {os.popen('free -m').read()}\n")
+        log_file.write(f"Hashcat terminou com o código de saída {process.returncode}\n")
         if process.returncode != 0:
             log_file.write("\nHashcat terminou com erros.\n")
 
